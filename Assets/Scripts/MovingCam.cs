45a82ef1a4963af8e31d4e2c13b3c5428ec3e1cf
Horror/Yazan <yazane701@gmail.com>
2022-05-15 21:59:01 +0300

    Switch Controls + Add Touch/Gamepad

--- Assets/Scripts/MovingCam.cs
+++ Assets/Scripts/MovingCam.cs
@@ -2,29 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.InputSystem;
 
 public class MovingCam : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera cam;
     [SerializeField] float camSpeed = 5;
     CinemachineTrackedDolly dolly;
+    MyPlayerInput myPlayerInput;
+    SwitchControlsType switchControls;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        myPlayerInput = new MyPlayerInput();
+        myPlayerInput.Movment.Enable();
+
+    }
     void Start()
     {
         dolly = cam.GetCinemachineComponent<CinemachineTrackedDolly>();
+        switchControls = GetComponent<SwitchControlsType>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if(myPlayerInput.Movment.enabled)
+        {
+            MovmentWithKeyBoard();
+
+        }
+        if (myPlayerInput.MovmentTouch.enabled)
+        {
+            MovmentWithTouch();
+
+        }
+
+    }
+    private void MovmentWithKeyBoard()
+    {
+        if (myPlayerInput.Movment.MoveForward.IsPressed())
         {
             dolly.m_PathPosition = Mathf.Clamp(dolly.m_PathPosition + camSpeed * Time.deltaTime, 0, dolly.m_Path.PathLength);
         }
-        if (Input.GetKey(KeyCode.S))
+
+
+        if (myPlayerInput.Movment.MoveBackward.IsPressed())
         {
-            dolly.m_PathPosition = Mathf.Clamp(dolly.m_PathPosition - camSpeed * Time.deltaTime, 0 ,dolly.m_Path.PathLength);
+            dolly.m_PathPosition = Mathf.Clamp(dolly.m_PathPosition - camSpeed * Time.deltaTime, 0, dolly.m_Path.PathLength);
         }
+    }
+    private void MovmentWithTouch()
+    {
+        if (myPlayerInput.MovmentTouch.MoveForward.IsPressed())
+        {
+            Vector2 lmao = myPlayerInput.MovmentTouch.MoveForward.ReadValue<Vector2>();
+            if(lmao.y >0.2)
+            {
+                dolly.m_PathPosition = Mathf.Clamp(dolly.m_PathPosition + camSpeed * Time.deltaTime, 0, dolly.m_Path.PathLength);
+            }
+            else if(lmao.y < -0.2f)
+            {
+                dolly.m_PathPosition = Mathf.Clamp(dolly.m_PathPosition - camSpeed * Time.deltaTime, 0, dolly.m_Path.PathLength);
+
+            }
+        }
+    }
+
+    public void Switch()
+    {
+        switchControls.Switch(myPlayerInput);
     }
     public void Telepoert(float pos)
     {
---
+++ Assets/Scripts/SwitchControlsType.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Cinemachine;
+
+public class SwitchControlsType : MonoBehaviour
+{
+    [SerializeField] CinemachineInputProvider mouseInputProvider;
+    [SerializeField] CinemachineInputProvider touchInputProvider;
+    CinemachinePOV camera;
+    MyPlayerInput myPlayerInput;
+    private void Start()
+    {
+        touchInputProvider.enabled = false;
+        mouseInputProvider.enabled = true;
+        camera = GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachinePOV>();
+    }
+    private void Update()
+    {
+        if (myPlayerInput == null) return;
+        if (myPlayerInput.MovmentTouch.enabled)
+        {
+            Vector2 delta = myPlayerInput.MovmentTouch.Look.ReadValue<Vector2>();
+            camera.m_VerticalAxis.Value += delta.y * 200 * Time.deltaTime;
+            camera.m_HorizontalAxis.Value += delta.x * 200 * Time.deltaTime;
+        }
+
+    }
+    public void Switch(MyPlayerInput myPlayerInput)
+    {
+        this.myPlayerInput = myPlayerInput;
+        if (myPlayerInput.Movment.enabled)
+        {
+            myPlayerInput.Movment.Disable();
+            myPlayerInput.MovmentTouch.Enable();
+            touchInputProvider.enabled = true;
+            mouseInputProvider.enabled = false;
+        }
+        else if(myPlayerInput.MovmentTouch.enabled)
+        {
+            myPlayerInput.MovmentTouch.Disable();
+            myPlayerInput.Movment.Enable();
+            touchInputProvider.enabled = false;
+            mouseInputProvider.enabled = true;
+
+        }
+    }
+}
